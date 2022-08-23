524c8d8673704be41e64fab6f49f2c6ceceafeb6
aerurine <vailett14@gmail.com>
2022-08-23 20:38:32 +0300

    fix: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,7 @@ module.exports = {
           { test: /\.svg$/, use: 'svg-inline-loader' },
           { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
           { test: /\.(js)$/, use: 'babel-loader' },
-          { test: /\.html$/, use: 'html-loader' },
-          { test: /\.(gif|png|jpe?g|svg)$/i, use: 'file-loader' },
+          { test: /\.(png|jpg|gif)$/i, use: 'url-loader' },
         ]
       },
     output: {
